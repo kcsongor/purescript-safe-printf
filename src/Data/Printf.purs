8474d69aa78c96f67b40bb724dff769f775aa4f3
justinwoo <moomoowoo@gmail.com>
2018-04-30 22:02:41 -0500

    compiler/0.12 changes

--- src/Data/Printf.purs
+++ src/Data/Printf.purs
@@ -1,42 +1,42 @@
 module Data.Printf where
 
-import Prelude (id, (<>), show)
-import Type.Prelude (SProxy (..), class IsSymbol, reflectSymbol)
-import Type.Data.Symbol (class ConsSymbol)
+import Prelude (identity, (<>), show)
+import Type.Prelude (class IsSymbol, SProxy(..), reflectSymbol)
+import Prim.Symbol as Symbol
 
 class Format (string :: Symbol) fun | string -> fun where
-  format :: @string -> fun
+  format :: SProxy string -> fun
 
 instance formatFFormat ::
   ( Parse string format
   , FormatF format fun
   ) => Format string fun where
-  format _ = formatF @format ""
+  format _ = formatF (FProxy :: FProxy format) ""
 
 class FormatF (format :: FList) fun | format -> fun where
-  formatF :: @format -> String -> fun
+  formatF :: FProxy format -> String -> fun
 
 instance formatFNil :: FormatF FNil String where
-  formatF _ = id
+  formatF _ = identity
 
 instance formatFConsD ::
   FormatF rest fun
   => FormatF (FCons D rest) (Int -> fun) where
   formatF _ str
-    = \i -> formatF @rest (str <> show i)
+    = \i -> formatF (FProxy :: FProxy rest) (str <> show i)
 
 instance formatFConsS ::
   FormatF rest fun
   => FormatF (FCons S rest) (String -> fun) where
   formatF _ str
-    = \s -> formatF @rest (str <> s)
+    = \s -> formatF (FProxy :: FProxy rest) (str <> s)
 
 instance formatFConsLit ::
   ( IsSymbol lit
   , FormatF rest fun
   ) => FormatF (FCons (Lit lit) rest) fun where
   formatF _ str
-    = formatF @rest (str <> reflectSymbol (SProxy :: SProxy lit))
+    = formatF (FProxy :: FProxy rest) (str <> reflectSymbol (SProxy :: SProxy lit))
 
 --------------------------------------------------------------------------------
 class Parse (string :: Symbol) (format :: FList) | string -> format
@@ -45,20 +45,22 @@ foreign import kind FList
 foreign import data FNil :: FList
 foreign import data FCons :: Specifier -> FList -> FList
 
+data FProxy (f :: FList) = FProxy
+
 instance aNilParse :: Parse "" (FCons (Lit "") FNil)
-else instance bConsParse :: (ConsSymbol h t string, Match h t fl) => Parse string fl
+else instance bConsParse :: (Symbol.Cons h t string, Match h t fl) => Parse string fl
 
 class Match (head :: Symbol) (tail :: Symbol) (out :: FList) | head tail -> out
 
 instance aMatchFmt :: Match a "" (FCons (Lit a) FNil)
 else instance bMatchFmt ::
-  ( ConsSymbol h t s
+  ( Symbol.Cons h t s
   , MatchFmt h spec
   , Parse t rest
   ) => Match "%" s (FCons (Lit "") (FCons spec rest))
 else instance cMatchFmt ::
   ( Parse s (FCons (Lit acc) r)
-  , ConsSymbol o acc rest
+  , Symbol.Cons o acc rest
   ) => Match o s (FCons (Lit rest) r)
 
 class MatchFmt (head :: Symbol) (out :: Specifier) | head -> out
